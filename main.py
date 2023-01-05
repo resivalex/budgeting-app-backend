9f0c8041f2b427d08808275c5287c0636788c3e7
Ivan Reshetnikov <resivalex@gmail.com>
2023-01-05 21:42:45 +0600

    Setup CORS

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from typing import List
 from dotenv import load_dotenv
 from budgeting_app_backend.transactions import Example
+from fastapi.middleware.cors import CORSMiddleware
 
 
 load_dotenv()
 
 
 app = FastAPI(docs_url='/api')
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/', tags=['System'])
