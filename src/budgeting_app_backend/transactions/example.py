71cc6feef0bb3ab0f7ff8269018de295921d4172
Ivan Reshetnikov <resivalex@gmail.com>
2023-01-08 12:38:28 +0600

    Enrich example data

--- src/budgeting_app_backend/transactions/example.py
+++ src/budgeting_app_backend/transactions/example.py
@@ -3,11 +3,43 @@ class Example:
     def all(self):
         return [
             {
-                'timestamp': 1672900704,
-                'amount': '2.34',
-                'currency': 'USD',
-                'account': 'Account',
-                'payee': 'Shop #1',
-                'comment': 'Fruits'
+                'datetime': '2023-01-06 09:59:59',
+                'account': 'Russian bank',
+                'category': 'Correction',
+                'type': 'income',
+                'amount': '3000.00',
+                'currency': 'RUB',
+                'payee': '',
+                'comment': 'Initial balance'
+            },
+            {
+                'datetime': '2023-01-07 10:00:00',
+                'account': 'Cash',
+                'category': 'Correction',
+                'type': 'income',
+                'amount': '10000.00',
+                'currency': 'KZT',
+                'payee': '',
+                'comment': 'Initial balance'
+            },
+            {
+                'datetime': '2023-01-07 10:07:30',
+                'account': 'Cash',
+                'category': '',
+                'type': 'transfer',
+                'amount': '5000.00',
+                'currency': 'KZT',
+                'payee': 'Bank card',
+                'comment': 'Initial balance'
+            },
+            {
+                'datetime': '2023-01-08 12:13:14',
+                'account': 'Bank card',
+                'category': 'Fastfood',
+                'type': 'expense',
+                'amount': '1950.00',
+                'currency': 'KZT',
+                'payee': 'KFC',
+                'comment': 'Basket'
             }
         ]
