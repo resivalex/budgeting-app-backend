0967445914b23c9ca9f8087261e04a3cc0942348
Ivan Reshetnikov <resivalex@gmail.com>
2025-04-28 00:07:28 +0500

    Remove dumps table

---
+++ alembic/versions/20250427_235855_remove_dumps_table.py
@@ -0,0 +1,24 @@
+"""remove_dumps_table
+
+Revision ID: 1d88274c2e8f
+Revises: 6feb07337ab4
+Create Date: 2025-04-27 23:58:55.392530
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "1d88274c2e8f"
+down_revision = "e36a2316e146"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
